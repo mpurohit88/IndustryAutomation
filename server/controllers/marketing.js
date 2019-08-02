fdd2564d7909775f8d84e364d5eadb0a6bfcf8c8
mukesh purohit <mpurohit88@gmail.com>
2019-08-02 08:54:42 +0530

    asdf

--- server/controllers/marketing.js
+++ server/controllers/marketing.js
@@ -2,6 +2,8 @@ const Marketing = require("../models/marketing")
 
 const getTemplate = function (req, res, next) {
   try {
+    const type = req.decoded.email.indexOf('export') > -1 ? parseInt(req.body.type) + 1 : req.body.type;
+
     new Marketing({}).GetTemplate(req.decoded.companyId).then(function (errorLogs) {
       res.send(errorLogs);
     });
